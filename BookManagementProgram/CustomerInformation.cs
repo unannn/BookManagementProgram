cfe86605175acfd69f8977bae73931e29fa26607
Lee Yun Hwan <zoo7930@naver.com>
2020-05-06 11:54:21 +0900

    고객 정보를 관리할 메소드를 담은 CustomerMangement 클래스 생성

---
+++ BookManagementProgram/CustomerInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagementProgram
+{
+    class CustomerInformation
+    {
+        private string id;
+        private string password;
+        private string name;
+        private string phoneNumber;
+        private string adress;
+
+        //게터세터, 생성자       
+    }
+    
+}
---
+++ BookManagementProgram/CustomerManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagementProgram
+{
+    class CustomerManagement
+    {
+        public string ModifyAdress(string adress)
+        {
+            string ModifiedAdress = null;
+
+
+            return ModifiedAdress;
+        }
+
+        public string ModifyPhoneNumber(string phoneNumber)
+        {
+            string ModifiedPhoneNumber = null;
+
+            return ModifiedPhoneNumber;
+        }
+
+        public List<CustomerInformation> DeleteCustomerData(List<CustomerInformation> custmerinformation)
+        {
+
+            return custmerinformation;
+        }
+
+        public List<CustomerInformation> SerchCustomer(List<CustomerInformation> customer,string serchString)
+        {
+            List<CustomerInformation> serchedCustomers = new List<CustomerInformation>();
+
+            return  serchedCustomers;
+        }
+        public void PrintAllCustomer(List<CustomerInformation> customers)
+        {
+
+        }
+    }
+}
